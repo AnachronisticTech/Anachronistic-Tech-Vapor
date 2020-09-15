6a1d11a800f0121848d3f8bb7158374604713a0d
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2020-09-15 18:28:45 +0300

    Upgraded to HTTP2

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,6 +2,7 @@ import Fluent
 import FluentMySQLDriver
 import Vapor
 import Leaf
+import NIOSSL
 
 // configures your application
 public func configure(_ app: Application) throws {
@@ -20,6 +21,29 @@ public func configure(_ app: Application) throws {
     ), as: .mysql)
     
     app.routes.defaultMaxBodySize = ByteCount(integerLiteral: 10240000)
+    
+    let homePath = Environment.get("CERTIFICATE_PATH") ?? app.directory.workingDirectory
+    let certPath = homePath + "/fullchain.pem"
+    let keyPath = homePath + "/privkey.pem"
+
+    let certs = try! NIOSSLCertificate.fromPEMFile(certPath)
+        .map { NIOSSLCertificateSource.certificate($0) }
+    let tls = TLSConfiguration.forServer(certificateChain: certs, privateKey: .file(keyPath))
+
+    app.http.server.configuration = .init(
+        hostname: "localhost",
+        port: 8080,
+        backlog: 256,
+        reuseAddress: true,
+        tcpNoDelay: true,
+        responseCompression: .disabled,
+        requestDecompression: .disabled,
+        supportPipelining: false,
+        supportVersions: Set<HTTPVersionMajor>([.two]),
+        tlsConfiguration: tls,
+        serverName: nil,
+        logger: nil
+    )
 
     // register routes
     try routes(app)
